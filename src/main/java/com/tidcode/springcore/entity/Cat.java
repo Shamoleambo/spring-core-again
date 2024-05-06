32c4117a5a63da58553b4a12e9dee0361afe97d9
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-05-06 12:18:39 -0300

    trovadorismo

--- src/main/java/com/tidcode/springcore/entity/Cat.java
+++ src/main/java/com/tidcode/springcore/entity/Cat.java
@@ -2,6 +2,9 @@ package com.tidcode.springcore.entity;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class Cat implements Animal {
 
@@ -11,5 +14,15 @@ public class Cat implements Animal {
 
 	public String speak() {
 		return "Meow meow!";
+	}
+	
+	@PostConstruct
+	public void doCatBegin() {
+		System.out.println("I'm gonna CAAAAAAT!");
+	}
+	
+	@PreDestroy
+	public void catsEnding() {
+		System.out.println("No wonder I'm the best, I've catted all over your face!");
 	}
 }
--- src/main/java/com/tidcode/springcore/entity/Dog.java
+++ src/main/java/com/tidcode/springcore/entity/Dog.java
@@ -3,8 +3,10 @@ package com.tidcode.springcore.entity;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class Dog implements Animal {
 
 	public Dog() {
@@ -13,6 +15,16 @@ public class Dog implements Animal {
 
 	public String speak() {
 		return "Au au!";
+	}
+	
+	@PostConstruct
+	public void doDogBegin() {
+		System.out.println("Let's au au, all over!");
+	}
+	
+	@PreDestroy
+	public void endDogness() {
+		System.out.println("I have aued enough!");
 	}
 
 }
