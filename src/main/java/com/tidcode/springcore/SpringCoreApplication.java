c0ad27e58dd4bf8021ed88f4712fd8e5d2b0b2d6
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-05-06 11:55:52 -0300

    and just like that I've learned the wonders of the @Lazy annotation and how it relates with the @Qualifier annotation

--- src/main/java/com/tidcode/springcore/SpringCoreApplication.java
+++ src/main/java/com/tidcode/springcore/SpringCoreApplication.java
@@ -2,6 +2,7 @@ package com.tidcode.springcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class SpringCoreApplication {
--- src/main/java/com/tidcode/springcore/entity/Dog.java
+++ src/main/java/com/tidcode/springcore/entity/Dog.java
@@ -1,10 +1,12 @@
 package com.tidcode.springcore.entity;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Lazy
 public class Dog implements Animal {
 
 	public Dog() {
